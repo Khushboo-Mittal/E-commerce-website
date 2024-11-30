1f8182b676d4092b22b0a3be6ade42261cb37d1d
Tanisha Priya <tanishapriya@Tanishas-MacBook-Air.local>
2024-11-30 23:00:56 +0530

    Updated order history files

--- js/order_history.js
+++ js/order_history.js
@@ -9,17 +9,5 @@ function redirectToRecommended() {
   // Retrieve the input values for username and password from the HTML form
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-
-  // Check if both username and password are provided
-  if (username && password) {
-      // Show a welcome alert to the user with their username
-      alert(`Welcome, ${username}! Redirecting to Recommended Page...`);
-      // Redirect the browser to the Recommended page
-      window.location.href = "../pages/recommended.html";
-      localStorage.setItem("username", username); // Save to localStorage
-  } else {
-      // Alert the user to provide valid credentials if either field is empty
-      alert("Please enter valid credentials.");
-  }
 }
 
